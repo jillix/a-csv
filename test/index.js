544d14d62c6e982ce0ed4c7729e01ae5d0c5289e
Ionică Bizău <bizauionica@yahoo.com>
2015-05-19 18:35:36 +0300

    Fixes

--- test/index.js
+++ test/index.js
@@ -1,37 +1,33 @@
 // Dependencies
 var Assert = require("assert")
   , CSV = require("../lib")
-  , Once = require("once")
   ;
-
-// Configurations
-Once.proto();
 
 // Expectations
 const EXPECT = {
-    HEADERS: [
-      ["Name", "Age", "Location"]
-    , ["Alice", "15", "Europe"]
-    , ["Bob", "17", "Asia"]
-    , ["Carol", "70", "Africa"]
-    ]
-    NO_HEADERS: [
-      ["Alice", "15", "Europe"]
-    , ["Bob", "17", "Asia"]
-    , ["Carol", "70", "Africa"]
-    ]
+  HEADERS: [
+    ["Name", "Age", "Location"]
+  , ["Alice", "15", "Europe"]
+  , ["Bob", "17", "Asia"]
+  , ["Carol", "70", "Africa"]
+  ]
+  , NO_HEADERS: [
+    ["Alice", "15", "Europe"]
+  , ["Bob", "17", "Asia"]
+  , ["Carol", "70", "Africa"]
+  ]
 };
 
 it("should parse the csv file, with default options", function (cb) {
     var rows = [];
-    cb.once();
-    CSV.parse(__dirname + "/no-headers.csv", function (err, row, next) {
+    CSV.parse(__dirname + "/headers.csv", function (err, row, next) {
 
         if (err) {
             return cb(err);
         }
 
         if (row === null) {
+            console.log(rows);
             Assert.deepEqual(rows, EXPECT.HEADERS);
             return cb();
         }
@@ -43,8 +39,7 @@ it("should parse the csv file, with default options", function (cb) {
 
 it("should parse the csv file, handling the headers", function (cb) {
     var rows = [];
-    cb.once();
-    CSV.parse(__dirname + "/no-headers.csv", { headers: true }, function (err, row, next) {
+    CSV.parse(__dirname + "/headers.csv", { headers: true }, function (err, row, next) {
 
         if (err) {
             return cb(err);
